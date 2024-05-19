6357d4188814aacd0c9161587e3e29f09d3bf60e
asrafulislamwd <asrafulislamwd@gmail.com>
2024-05-20 02:53:30 +0600

    popular

--- src/Shared/NavBar/NavBar.jsx
+++ src/Shared/NavBar/NavBar.jsx
@@ -1,17 +1,9 @@
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
 
   const navoptione = <>
-    <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
+      <li><Link to="/menu">Our Menu</Link></li>
       <li><a>Item 3</a></li>
   </>
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,12 @@ import {
 } from "react-router-dom";
 
 import { router } from './Routes/Routes';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
   </React.StrictMode>,
 )
